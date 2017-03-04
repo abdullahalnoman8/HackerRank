e9b98f9893774e8ffea5cd9e248015cb5152f948
Opu <opucse8@gmail.com>
2017-03-04 12:26:50 +0600

    ## ArrayFirst

--- HackerRank/src/com/datastructure/arrays/Arrays.java
+++ HackerRank/src/com/datastructure/arrays/Arrays.java
@@ -4,5 +4,7 @@ package com.datastructure.arrays;
  * Created by Opu on 3/4/2017.
  */
 public class Arrays {
-
+   public static void main(String args[]){
+       System.out.println("Working .....");
+   }
 }
